ca15d4c3de75507ec74a91f4f7e5005b760af7b2
longshaoqing <88479838+longshaoqing@users.noreply.github.com>
2022-04-28 10:51:16 +0800

    Update test_name_function.py

--- Homework/chapter11/test_name_function.py
+++ Homework/chapter11/test_name_function.py
@@ -4,5 +4,7 @@ class NameTest(unittest.TestCase):
     def test_name(self):
         name=get_name("Long","Shaoqing")
         self.assertEqual(name,"Long Shaoqing")
+     def ni():
+        print("我是")
 if __name__ == '__main__':
     unittest.main
